ddc11b4c1afb510d2c8be11d76d673bb492cad16
Enzo Bassot <enzo.bassot@gmail.com>
2023-02-12 23:05:05 +0100

    mood changed + background music

---
+++ src/script/pages/home/script.js
@@ -0,0 +1,18 @@
+let backgroundMusic = document.querySelector('#bgmusic');
+let playBackgroundMusic = document.querySelector('#bgmusic-play');
+let stopBackgroundMusic = document.querySelector('#bgmusic-stop');
+
+backgroundMusic.volume = 0.007;
+backgroundMusic.play();
+
+playBackgroundMusic.addEventListener("click", () => {
+    backgroundMusic.play();
+    playBackgroundMusic.classList.add("hidden");
+    stopBackgroundMusic.classList.remove("hidden");
+})
+
+stopBackgroundMusic.addEventListener("click", () => {
+    backgroundMusic.pause();
+    stopBackgroundMusic.classList.add("hidden");
+    playBackgroundMusic.classList.remove("hidden");
+})
