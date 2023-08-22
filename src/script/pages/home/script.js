5f0470d502f052bf0218c588fdc77116ccada73f
Mrzozo (Enzo Bassot) <enzo.bassot@gmail.com>
2023-08-22 04:28:00 +0200

    0.22: Random featured video + Some graphics reworks + New pagegit add * Art gallery (under construction)

--- src/script/pages/home/script.js
+++ src/script/pages/home/script.js
@@ -1,3 +1,4 @@
+// background music
 let backgroundMusic = document.querySelector('#bgmusic');
 let playBackgroundMusic = document.querySelector('#bgmusic-play');
 let stopBackgroundMusic = document.querySelector('#bgmusic-stop');
@@ -16,3 +17,43 @@ stopBackgroundMusic.addEventListener("click", () => {
     stopBackgroundMusic.classList.add("hidden");
     playBackgroundMusic.classList.remove("hidden");
 })
+
+// randomized youtube video
+const videos = [
+    {
+        name: "Two time by RAMDARAM",
+        id: "_LLCz1FCWrY"
+    },
+    {
+        name: "Ena Dream BBC Trailer",
+        id: "qLurAhsqXWc"
+    },
+    {
+        name: "Ghost Of The Year",
+        id: "1KxLntBdKQI"
+    },
+    {
+        name: "Time Adventure singed by Rebecca Sugar",
+        id: "YGZbDDJ8Cnk"
+    },
+    {
+        name: "Captain Yajima by Worthikids",
+        id: "l-TJm7HkzkQ"
+    },
+    {
+        name: "Rain Sound 10h",
+        id: "mPZkdNFkNps"
+    },
+    {
+        name: "Pixel Galaxy by Snail's House",
+        id: "3nlSDxvt6JU"
+    },
+    {
+        name: "Ena Extinction Party",
+        id: "Td7CBNu0914"
+    }
+]
+
+const iframe = document.querySelector('#randomized-video');
+const currentVideoId = videos[Math.floor(Math.random() * videos.length)].id;
+iframe.src = `https://www.youtube.com/embed/${currentVideoId}`;
